3650fcad710a91d8a52cce1c6b7b56efebc2b7a2
193diego <100795325+193diego@users.noreply.github.com>
2023-12-27 22:50:21 -0500

    Add files via upload

---
+++ Datos/Datos.cs
@@ -0,0 +1,24 @@
+﻿using Bohorquez;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class Datos
+    {
+        public bool VerificarUsuario(string nombreUsuario, string clave)
+        {
+            using (SqlConnection cn = conexion.conectar())
+            {
+                using (SqlCommand cmd = new SqlCommand("VERIFICAR_USUARIO", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@NOMBRE_USUARIO", nombreUsuario);
+                    cmd.Parameters.AddWithValue("@CLAVE", clave);
+                    object existe = cmd.ExecuteScalar();
+                    return existe != null && (int)existe == 1;
+                }
+            }
+        }
+    }
+}
