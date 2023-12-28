e24800e3de9bde89c020d1c5b42c8efd676b6d97
193diego <100795325+193diego@users.noreply.github.com>
2023-12-27 22:52:10 -0500

    Add files via upload

---
+++ Logica/Logica.cs
@@ -0,0 +1,13 @@
+﻿namespace Logica
+{
+    public class Logica
+    {
+        private Datos.Datos datosUsuario = new Datos.Datos();
+
+        public bool AutenticarUsuario(string nombreUsuario, string clave)
+        {
+            return datosUsuario.VerificarUsuario(nombreUsuario, clave);
+        }
+    }
+
+}
