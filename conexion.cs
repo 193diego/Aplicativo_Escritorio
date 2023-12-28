71db649fa387dc1b230db4eb23769d3997b3f8ea
Diego <diego.carvajal.gomez@uagraria.edu.ec>
2023-12-27 22:24:49 -0500

    Funciona el Login

---
+++ conexion.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace Bohorquez
+{
+    class conexion
+    {
+        public static SqlConnection conectar()
+        {
+            SqlConnection cn = new SqlConnection("server=LAPTOP-P821MMUI;database=CAMISAS_POLOS;integrated security=true;");
+            cn.Open();
+            return cn;
+        }
+    }
+}
