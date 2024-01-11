f8a6a0aaa05f33c35a77031b0af91da15b78142e
193diego <100795325+193diego@users.noreply.github.com>
2024-01-11 15:47:07 -0500

    Add files via upload

--- Datos/Datos.cs
+++ Datos/Datos.cs
@@ -1,4 +1,5 @@
 ﻿using Bohorquez;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -21,4 +22,163 @@ namespace Datos
             }
         }
     }
+    public class DatosOrdenProduccion
+    {
+        public static DataTable ObtenerOrdenesProduccion()
+        {
+            using (SqlConnection cn = conexion.conectar())
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM ORDEN_PRODUCCION", cn))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+        public static void EliminarOrdenProduccion(int numeroOrden)
+        {
+            using (SqlConnection cn = conexion.conectar())
+            {
+                using (SqlCommand cmd = new SqlCommand("ELIMINAR_ORDEN_PRODUCCION", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@NUMERO_ORDEN", numeroOrden);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public static void InsertarOrdenProduccion(string nombreUsuario, int numeroOrden, int cantidad, DateTime fechaInicio, DateTime fechaEntrega, string modelo, string talla, string color, string estadoProduccion, int idMaterial)
+        {
+            using (SqlConnection cn = conexion.conectar())
+            {
+                using (SqlCommand cmd = new SqlCommand("INSERTAR_ORDEN_PRODUCCION", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@NOMBRE_USUARIO", nombreUsuario);
+                    cmd.Parameters.AddWithValue("@NUMERO_ORDEN", numeroOrden);
+                    cmd.Parameters.AddWithValue("@CANTIDAD", cantidad);
+                    cmd.Parameters.AddWithValue("@FECHA_INICIO", fechaInicio);
+                    cmd.Parameters.AddWithValue("@FECHA_ENTREGA", fechaEntrega);
+                    cmd.Parameters.AddWithValue("@MODELO", modelo);
+                    cmd.Parameters.AddWithValue("@TALLA", talla);
+                    cmd.Parameters.AddWithValue("@COLOR", color);
+                    cmd.Parameters.AddWithValue("@ESTADO_PRODUCCION", estadoProduccion);
+                    cmd.Parameters.AddWithValue("@ID_MATERIAL", idMaterial);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public static void EditarOrdenProduccion(string nombreUsuario, int numeroOrden, int cantidad, DateTime fechaInicio, DateTime fechaEntrega, string modelo, string talla, string color, string estadoProduccion, int idMaterial, int idOrdenProduccion)
+        {
+            using (SqlConnection cn = conexion.conectar())
+            {
+                using (SqlCommand cmd = new SqlCommand("EDITAR_ORDEN_PRODUCCION", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@NOMBRE_USUARIO", nombreUsuario);
+                    cmd.Parameters.AddWithValue("@NUMERO_ORDEN", numeroOrden);
+                    cmd.Parameters.AddWithValue("@CANTIDAD", cantidad);
+                    cmd.Parameters.AddWithValue("@FECHA_INICIO", fechaInicio);
+                    cmd.Parameters.AddWithValue("@FECHA_ENTREGA", fechaEntrega);
+                    cmd.Parameters.AddWithValue("@MODELO", modelo);
+                    cmd.Parameters.AddWithValue("@TALLA", talla);
+                    cmd.Parameters.AddWithValue("@COLOR", color);
+                    cmd.Parameters.AddWithValue("@ESTADO_PRODUCCION", estadoProduccion);
+                    cmd.Parameters.AddWithValue("@ID_MATERIAL", idMaterial);
+                    cmd.Parameters.AddWithValue("@ID_ORDEN_PRODUCCION", idOrdenProduccion);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public static DataTable BuscarOrdenProduccion(string caracter)
+        {
+            using (SqlConnection cn = conexion.conectar())
+            {
+                using (SqlCommand cmd = new SqlCommand("BUSCAR_ORDEN_PRODUCCION", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@CARACTER", caracter);
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    return dataTable;
+                }
+            }
+        }
+    }
+    public class DatosMaterial
+    {
+        public static DataTable ObtenerMateriales()
+        {
+            using (SqlConnection cn = conexion.conectar())
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT ID_MATERIAL, NOMBRE_MATERIAL FROM MATERIALES", cn))
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+    }
+    // Tabla Materiales
+    public class DatosMateriales
+    {
+        public static DataTable ObtenerMaterialesI()
+        {
+            using (SqlConnection cn = conexion.conectar())
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM MATERIALES", cn))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+    }
+    //Tabla Produccion
+    public class DatosProduccion
+    {
+        public static DataTable ObtenerProduccion()
+        {
+            using (SqlConnection cn = conexion.conectar())
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM PRODUCCION", cn))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+    }
+    //
+    public class DatosControlCalidad
+    {
+        public static DataTable ObtenerControlCalidad()
+        {
+            using (SqlConnection cn = conexion.conectar())
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM CONTROL_CALIDAD", cn))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+    }
 }
+
